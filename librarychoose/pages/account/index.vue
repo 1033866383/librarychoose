ef6c44c530fa57f565f3691c97b29b92c5088664
fanbozhou <1033866383@qq.com>
2022-03-07 23:50:16 +0800

    init web

---
+++ librarychoose/pages/account/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<my-custom bgColor="bg-gradual-blue" :isBack="false">
+			<block slot="content">我的</block>
+		</my-custom>
+		我的
+		<my-component-tabar />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ librarychoose/pages/home/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="pages">
+    <my-custom bgColor="bg-gradual-blue" :isBack="false">
+    	<block slot="content">首页</block>
+    </my-custom>
+    <view class="content">
+     Home
+    </view>
+		<my-component-tabar />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+    mounted() {
+      if (!this.vuex_hasLogin) {
+        this.$u.route('/pages/login/login/index')
+      }
+    },
+    methods: {
+      navTo(url,params) {
+        if (url) {
+          this.$u.route(url, params)
+        }
+      }
+    }
+	}
+</script>
+
+<style lang="scss" scoped>
+  .pages {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
