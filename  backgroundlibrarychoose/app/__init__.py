55bd4653e28564772a456ac4316215e20e8479de
bozhou.fan <bozhou.fan>
2022-03-08 20:26:00 +0800

    add user

---  backgroundlibrarychoose/app/__init__.py
+++  backgroundlibrarychoose/app/__init__.py
@@ -2,10 +2,14 @@ import logging.config
 import os
 
 from flask import Flask
+
 from flask_cors import CORS
+from flask_rbac import RBAC
 from config import dict_logger
 from .userservices import user_service
 from .seatservices import seat_service
+
+rbac = RBAC()
 
 
 def create_app(config_name):
@@ -19,4 +23,6 @@ def create_app(config_name):
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.register_blueprint(seat_service, url_prefix='/seat/')
     app.register_blueprint(user_service, url_prefix='/user/')
+    app.config['RBAC_USE_WHITE'] = True
+    rbac.init_app(app)
     return app
---  backgroundlibrarychoose/app/dao/base.py
+++  backgroundlibrarychoose/app/dao/base.py
@@ -1,27 +1,31 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 __all__ = ['engine']
-# 连接数据库
+# connect db
 engine = create_engine('sqlite:///librarychoose.db', echo=True)
 
-# 基本类
+# base
 Base = declarative_base()
 
 
 # 表要继承基本类
 class User(Base):
-    __tablename__ = 'users'  # 表的名字
+    __tablename__ = 'users'
 
-    # 定义各字段
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(11), unique=True)
+    name = Column(String(55))
+    password = Column(String(50), unique=True)
+    email = Column(String(100), unique=True)
+    iphone = Column(String(15))
+    info = Column(Text) #可配置参数，例如头像等其他可变参数
 
-    def __str__(self):
-        return self.id
+
+class Permission(object):
+    COMMENT = 0x02
+    ADMINISTER = 0x80
 
 
 if __name__ == '__main__':
