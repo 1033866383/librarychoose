3cb1f23e8d27bc21c69f33e426d126c2a0851a52
bozhou.fan <bozhou.fan>
2022-03-10 17:06:33 +0800

    add alluserinfo

---
+++  backgroundlibrarychoose/app/userservices/implements/alluserinfo.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import current_app, request
+from flask_restful import Resource
+
+from app.dao.base import User, connect, alchemy2json
+from app.util.jwtutil import verify_jwt
+from app.util.response import response
+
+
+class AllUserInfo(Resource):
+
+    def get(self):
+        current_app.logger.info("all userinfo info {0}".format(request.json))
+        token = request.headers.get("token")
+        token = verify_jwt(token)
+        current_app.logger.info("token {0}".format(token))
+        with connect() as session:
+            user = session.query(User).all()
+            res = alchemy2json(user)
+            return response(res)
---  backgroundlibrarychoose/app/userservices/route.py
+++  backgroundlibrarychoose/app/userservices/route.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 
 from app.userservices import user_service
+from app.userservices.implements.alluserinfo import AllUserInfo
 from app.userservices.implements.login import Login
 from app.userservices.implements.register import Register
 from app.userservices.implements.updateinfo import UpdateInfo
@@ -13,3 +14,4 @@ api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(UserInfo, '/userinfo', endpoint='userinfo')
 api.add_resource(UpdateInfo, '/updateinfo', endpoint='updateinfo')
 api.add_resource(Upload, '/upload', endpoint='upload')
+api.add_resource(AllUserInfo, '/alluserinfo', endpoint='alluserinfo')
