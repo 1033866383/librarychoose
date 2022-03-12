9388ccf0676dbe0e1cc72de46963921b4f2a1227
fanbozhou <1033866383@qq.com>
2022-03-12 17:03:09 +0800

    add multi seat

---  backgroundlibrarychoose/app/seatservices/implments/addseat.py
+++  backgroundlibrarychoose/app/seatservices/implments/addseat.py
@@ -22,18 +22,36 @@ class AddSeat(Resource):
             """
             {'local': "A", 'left': 0, 'top': 0 }
             """
-            return response(status_code=500, msg="illegal param 错误的位置信息")
+            if isinstance(position, list):
+                if len(position) <= 0:
+                    return response(status_code=500, msg="illegal param 错误的位置信息")
+                elif not isinstance(position[0], dict):
+                    return response(status_code=500, msg="illegal param 错误的位置信息")
+
+            else:
+                return response(status_code=500, msg="illegal param 错误的位置信息")
         self.add_price(price)
         with connect() as session:
             library_item = session.query(Library).filter(Library.id == library).first()
             if not library_item:
                 return response(status_code=500, msg="illegal param 没有此自习室")
-            seat_item = session.query(Seat).filter(Seat.library == library).filter(Seat.position == json.dumps(position)).first()
-            if seat_item:
-                return response(status_code=500, msg="当前座位已被添加")
-            seat_item = Seat(library=library, position=json.dumps(position), price=json.dumps(price))
-            session.add(seat_item)
-            return response()
+            if isinstance(position, dict):
+                seat_item = session.query(Seat).filter(Seat.library == library).filter(
+                    Seat.position == json.dumps(position)).first()
+                if seat_item:
+                    return response(status_code=500, msg="当前座位已被添加")
+                seat_item = Seat(library=library, position=json.dumps(position), price=json.dumps(price))
+                session.add(seat_item)
+            elif isinstance(position, list):
+                data = ""
+                for item in position:
+                    seat_item = session.query(Seat).filter(Seat.library == library).filter(
+                        Seat.position == json.dumps(item)).first()
+                    if seat_item:
+                        data += "座位{0}-{1}-{2}已被添加;".format(item.get("local"), item.get("left"), item.get("right"))
+                    seat_item = Seat(library=library, position=json.dumps(item), price=json.dumps(price))
+                    session.add(seat_item)
+            return response(data)
 
     def add_price(self, price):
         if not price:
