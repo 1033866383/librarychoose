5f4b13bc283b9e13f2df5bc38163c19b2c6b9df4
fanbozhou <1033866383@qq.com>
2022-03-16 23:08:25 +0800

    add

---
+++ librarychoose/api/seat.js
@@ -0,0 +1,26 @@
+import { request } from './server.js'
+
+export const AllLibraryInfo = 
+function() {
+    return request.get('/seat/alllibraryinfo')
+    .then(data=>{return data.data})
+}
+
+export const AddLibrary = 
+function(param) {
+    return request.post('/seat/addlibrary',param)
+    .then(data=>{return data.data})
+}
+
+
+export const AllSeatInfo = 
+function(id, library) {
+    return request.get('/seat/allseatinfo?id='+id+"&library="+library)
+    .then(data=>{return data.data})
+}
+
+export const AddSeat = 
+function(param) {
+    return request.post('/seat/addseat?library',param)
+    .then(data=>{return data.data})
+}
