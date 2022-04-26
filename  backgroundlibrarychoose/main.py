e726599fabe786b516f21bdb0dd3913ff9a457f0
fanbozhou <1033866383@qq.com>
2022-04-26 23:08:03 +0800

    add role

---  backgroundlibrarychoose/main.py
+++  backgroundlibrarychoose/main.py
@@ -1,10 +1,14 @@
 import os
+import subprocess
+
+import schedule
 
 from app import create_app
 
 app = create_app(os.getenv('FLASK_ENV') or 'development')
 
 if __name__ == '__main__':
+    schedule.every().day.at("23:06").do(lambda: subprocess.run("librarychoosetask_jar/start.bat"))
     app.run(host="0.0.0.0", port=8071)
 
     # Base.metadata.create_all(engine)
